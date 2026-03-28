90040c05399c3e70594fefca6e2e4ef42f929192
Denis Baranov <125142326+Denisonya@users.noreply.github.com>
2026-03-28 22:05:25 +0300

    The configuration is set up to work with SqlAlchemy and the Postgresql database (#23)
    * feat: update requirements.txt
    
    * feat: add database.py to config BD connection
    
    * feat: add note model
    
    * feat: created notes table in DB
    
    * feat: simplify working with the database using dependency injection (get_db) in FastAPI
    
    * feat: implement /notes endpoint
    
    * feat: implement GET /notes endpoint
    
    * feat: update RELEASE.md PR template

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from .database import engine, SessionLocal
+from .models import Base, Note
+
+# создание базы данных и таблиц по метаданным моделей - только для разработки
+# (если база данных и все необходимые таблицы уже имеются, то метод не создает заново таблицы)
+Base.metadata.create_all(bind=engine)  # bind принимает класс, который используется для подключения к базе данных
+
+
+# определяем зависимость через которую объект сессии базы данных будет передаваться в функции обработки
+def get_db():
+    db = SessionLocal()  # создаем объект сессии базы данных
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 app = FastAPI()
 
@@ -6,3 +24,18 @@ app = FastAPI()
 @app.get("/")  # get, post, put, delete, patch
 async def root():
     return {"message": "Hello from Algo-Notes"}
+
+
+@app.post("/notes")
+async def create_note(title: str, content: str, db: Session = Depends(get_db)):
+    note = Note(title=title, content=content)
+    db.add(note)
+    db.commit()
+    db.refresh(note)
+    return note
+
+
+@app.get("/notes")
+def get_notes(db: Session = Depends(get_db)):
+    notes = db.query(Note).all()
+    return notes
