110e78bf63d9641b2d812cf114a6eeccc0b720f8
Denis Baranov <125142326+Denisonya@users.noreply.github.com>
2026-03-29 00:54:08 +0300

    Add categories, relationships, and structured notes system (#35)
    * feat: update requirements.txt
    
    * feat: add database.py to config BD connection
    
    * feat: add note model
    
    * feat: created notes table in DB
    
    * feat: simplify working with the database using dependency injection (get_db) in FastAPI
    
    * feat: implement /notes endpoint
    
    * feat: implement GET /notes endpoint
    
    * feat: update RELEASE.md PR template
    
    * feat: add Category model and relationships for Category model and Note model (#25)
    
    * feat: add endpoint POST /categories (#27)
    
    * feat: update POST /notes for working with categories (#29)
    
    * feat: update GET /notes for filtering by category (#32)
    
    * feat: add endpoint GET /categories (#34)
    
    * fix: update functions for async processing

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,11 +1,28 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 
 
-# создаем модель, объекты которой будут храниться в бд
+# параметр back_populates представляет атрибут связанной модели, с которой будет сопоставляться текущая модель
+# делаем связь Category.notes -- Note.category
+
+# создаем модель категории, объекты которой будут храниться в бд
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+
+    notes = relationship("Note", back_populates="category")  # связь с моделью Note через ее атрибут category
+
+
+# создаем модель заметки, объекты которой будут храниться в бд
 class Note(Base):
     __tablename__ = "notes"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
+
+    category_id = Column(Integer, ForeignKey("categories.id"))  # внешний ключ на столбец id из таблицы "categories"
+    category = relationship("Category", back_populates="notes")  # связь с моделью Category через ее атрибут notes
