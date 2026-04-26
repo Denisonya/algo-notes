24a3035cd9e64688bc2d8215a3abc465fd16a3ed
Denisonya <denisba2006@icloud.com>
2026-04-26 16:25:24 +0300

    feat: update note model and category model

--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,10 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
 
 
+# Связь User -> Category (1:N)
+# Связь User -> Note (1:N)
+
+# Модель пользователя, объекты которой будут храниться в БД
 class User(Base):
     __tablename__ = "users"
 
@@ -13,12 +17,15 @@ class User(Base):
 
     categories: Mapped[list["Category"]] = relationship(  # type: ignore
         "Category",
-        back_populates="user",
+        back_populates="user",  # связь с моделью Category через ее атрибут category
         cascade="all, delete-orphan"
     )
 
     notes: Mapped[list["Note"]] = relationship(  # type: ignore
         "Note",
-        back_populates="user",
+        back_populates="user",  # связь с моделью Note через ее атрибут user
         cascade="all, delete-orphan"
     )
+
+# Примечание:
+# Параметр back_populates представляет атрибут связанной модели, с которой будет сопоставляться текущая модель
