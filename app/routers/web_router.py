2823890d68f511e81e948834aa1b0ed009d3998d
Denisonya <denisba2006@icloud.com>
2026-04-26 21:04:04 +0300

    feat: update web_router.py

--- app/routers/web_router.py
+++ app/routers/web_router.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, Request, Depends, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from app.dependencies.db import get_db
+from app.schemas.user import UserCreate
+from app.services.auth_service import register_user_service
+from app.core.exceptions import AlreadyExistsError
 
 router = APIRouter(tags=["Web"])
-
 templates = Jinja2Templates(directory="app/templates")
 
 
@@ -13,3 +18,40 @@ def home(request: Request):
         "index.html",
         {"request": request}
     )
+
+
+@router.get("/register", response_class=HTMLResponse)
+def register_page(request: Request):
+    return templates.TemplateResponse(
+        "register.html",
+        {
+            "request": request,
+            "error": None
+        }
+    )
+
+
+@router.post("/register", response_class=HTMLResponse)
+def register_submit(
+    request: Request,
+    username: str = Form(...),
+    password: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    try:
+        data = UserCreate(username=username, password=password)
+        register_user_service(data, db)
+
+        return RedirectResponse(
+            url="/login",
+            status_code=303
+        )
+
+    except AlreadyExistsError as e:
+        return templates.TemplateResponse(
+            "register.html",
+            {
+                "request": request,
+                "error": str(e)
+            }
+        )
